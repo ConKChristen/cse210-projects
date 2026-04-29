388c36c85ec25fcf9b183a4ec3398cb2399e2883
ConKChristen <connork3163@gmail.com>
2026-04-29 13:26:35 -0600

    Complete Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool correct = false;
+        Console.WriteLine("Guess the random number between 1 and 100");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        while (correct == false)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+                Console.Write("Your guess is too high. Try again");
+            }
+            if (guess < number)
+            {
+                Console.Write("Your guess is too low. Try again");
+            }
+            if (guess == number)
+            {
+                Console.Write("You guessed correctly!");
+                correct = true;
+            }
+        }
     }
 }
